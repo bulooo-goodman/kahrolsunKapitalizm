b6f24bb6f797a403d6b3badbfb9aa3d5dbfdd35d
bulooo the goodman <123685261+bulooo-goodman@users.noreply.github.com>
2024-08-06 23:55:50 +0300

    test
    test

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title_test');
+    const subtitle = document.getElementById('subtitle');
+
+    title.classList.add('fade-in');
+    subtitle.classList.add('fade-in');
+});
